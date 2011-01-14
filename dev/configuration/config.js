87199ac2f55ad59481b762227a8a7e60d3eab6e2
soe soe <khinsoesoekyaw@gmail.com>
2011-01-14 14:21:39 +1100

    SAKIII-2165 making sure that the configuration file refelects the url mapping for anonymous users and logged in users

--- dev/configuration/config.js
+++ dev/configuration/config.js
@@ -738,12 +738,12 @@ sakai.config = {
     /*
      * List of pages that require a logged in user
      */
-    requireUser: ["/dev/my_sakai.html", "/dev/account_preferences.html", "/dev/group_edit.html", "/dev/inbox.html", "/dev/profile_edit.html"],
+    requireUser: ["/home", "/preferences", "/group/edit", "/inbox", "/profile/edit"],
 
     /*
      * List of pages that require an anonymous user
      */
-    requireAnonymous: ["/dev/index.html", "/dev/create_new_account.html", "/dev/", "/dev", "/", "/index.html"],
+    requireAnonymous: ["/index", "/register", "/", "/index"],
     /*
      * List of pages that will be added to requireUser if
      * anonAllowed is false
